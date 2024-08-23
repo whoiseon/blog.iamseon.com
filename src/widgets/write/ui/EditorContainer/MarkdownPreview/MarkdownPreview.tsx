ada7f6e948c49ad682be8cce0821fa2522b76a92
Inseon Hwang <whois__@naver.com>
2024-08-23 10:25:01 +0900

    fixed: markdown preview scroll bug

--- src/widgets/write/ui/EditorContainer/MarkdownPreview/MarkdownPreview.tsx
+++ src/widgets/write/ui/EditorContainer/MarkdownPreview/MarkdownPreview.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { useEffect, useRef } from 'react';
 import MarkdownRender from '@/src/features/post/ui/MarkdownRender';
 
 interface Props {
@@ -8,24 +7,10 @@ interface Props {
 }
 
 function MarkdownPreview({ markdown }: Props) {
-  const Wrapper = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    if (
-      Wrapper.current &&
-      Wrapper.current.scrollHeight > Wrapper.current.clientHeight
-    ) {
-      const scrollHeight = Wrapper.current.scrollHeight;
-      const clientHeight = Wrapper.current.clientHeight;
-      Wrapper.current.scrollTop = scrollHeight - clientHeight;
-    }
-  }, [markdown]);
-
   return (
     <div
       id="preview"
       className="pt-[2.5rem] pb-[4rem] px-[5.75rem] flex-1 overflow-y-auto custom-scroll-bar animate-fadeIn"
-      ref={Wrapper}
     >
       <MarkdownRender markdown={markdown} />
     </div>
