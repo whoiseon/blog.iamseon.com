b4c1883faa13108326a3983f584175706c211b79
Inseon Hwang <whois__@naver.com>
2024-09-04 18:00:18 +0900

    fix: modify the post date string transformation type in the sitemap generator

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -9,8 +9,8 @@ export default async function sitemap() {
   const posts = postList.payload?.list.map((post) => ({
     url: `${siteUrl}/post/${post.urlSlug}`,
     lastModified:
-      new Date(post.updatedAt).toDateString().split('T')[0] ??
-      new Date(post.createdAt).toDateString().split('T')[0],
+      new Date(post.updatedAt).toISOString().split('T')[0] ??
+      new Date(post.createdAt).toISOString().split('T')[0],
   }));
 
   const series = seriesList.payload?.map((item) => ({
